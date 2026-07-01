38e84c1167e0ae1736862e0df72c8b942f394d29
WD-93 <9657937+WD-93@users.noreply.github.com>
2026-07-01 12:39:42 +0200

    Fixed peephole bug
    In
    "
    lab1:
    push0
    lab2:
    jumdest
    "
    lab2 was being incorrectly moved to the same position as lab1.

--- Opt/Peephole.hs
+++ Opt/Peephole.hs
@@ -5,6 +5,8 @@ import Asm
 
 import Data.Map (Map(..))
 import qualified Data.Map as M
+import Data.Set (Set(..))
+import qualified Data.Set as S
 
 --This module is for optimizing generated assembly ([Asm]), not Core.
 --The treegraph stack instruction scheduler has a problem: trees are currently
@@ -47,36 +49,38 @@ type Constant = [Asm] --Bytes and UseLabels, equivalent to Serialized.
 --A normalized instruction sequence effect: a list of vars and constants are
 --prepended to the stack. We also need to keep track of placed labels to
 --prevent the linker from complaining.
-type Normalized = (Int, [LogicalWord], [Label])
+type Normalized = (Int, [LogicalWord], Set Label)
 emptyNorm :: Normalized
-emptyNorm = (0,[],[])
+emptyNorm = (0,[],S.empty)
 --Peepholable ops
 data POp = PPush Int Constant
          | PDup Int --1-indexed, points into the current stack
          | PSwap Int --0-indexed > 0, points into the current stack
          | PPop
-         | PPlaceLabel Label
+        -- | PPlaceLabel Label
   deriving (Eq,Ord,Read,Show)
-tryApplyPOp :: Normalized -> POp -> Maybe Normalized
-tryApplyPOp (height,ws,labs) = \case
-  PPush len const -> Just (height+1,LPush len const : ws, labs)
-  --Dup ixth elem of ws ++ LDup 0..
-  PDup ix ->
-    let dup'd = (ws ++ map LDup [0..]) !! ix
-    in Just (height+1,dup'd:ws,labs)
-  PSwap ix ->
-    if ix >= height
-    then Nothing
-    else let a:prefix = take ix ws
-             b:suffix = drop ix ws
-         in Just (height,b:prefix ++ a:suffix,labs)
-  PPop
-    | height == 0 -> Nothing
-    | let -> Just (height-1,tail ws,labs)
-  PPlaceLabel lab -> Just (height,ws,lab:labs)
+tryApplyPOp :: Normalized -> Set Label -> POp -> Maybe Normalized
+tryApplyPOp (height,ws,oldLabs) newLabs =
+  let labs = S.union oldLabs newLabs
+  in \case
+    PPush len const -> Just (height+1,LPush len const : ws, labs)
+    --Dup ixth elem of ws ++ LDup 0..
+    PDup ix ->
+      let dup'd = (ws ++ map LDup [0..]) !! ix
+      in Just (height+1,dup'd:ws,labs)
+    PSwap ix ->
+      if ix >= height
+      then Nothing
+      else let a:prefix = take ix ws
+               b:suffix = drop ix ws
+           in Just (height,b:prefix ++ a:suffix,labs)
+    PPop
+      | height == 0 -> Nothing
+      | let -> Just (height-1,tail ws,labs)
+    --PPlaceLabel lab -> Just (height,ws,lab:labs)
 compileNormalized :: Normalized -> [Asm]
 compileNormalized (height,lws,labs) =
-  map PlaceLabel labs ++
+  map PlaceLabel (S.toList labs) ++
   go 0 M.empty (reverse lws)
   where
     go :: Int -> --current stack height - original
@@ -114,8 +118,8 @@ peephole = go emptyNorm
       [] -> compileNormalized norm
       asms@(asm:asms') ->
         case parsePOp asms of
-          Just (pOp,asms'')
-            | Just norm' <- tryApplyPOp norm pOp ->
+          Just (pOp,labs,asms'')
+            | Just norm' <- tryApplyPOp norm labs pOp ->
               go norm' asms''
           --Flush the accumulated words, skip non-POp asm, continue
           _ -> compileNormalized norm ++ asm : peephole asms'
@@ -124,26 +128,59 @@ peephole = go emptyNorm
 --a maybe.
 --I treat Push and PushLabel as not peepholable, but that's fine
 --since EVMC doesn't emit them. I also don't emit Dup or Swap.
-parsePOp :: [Asm] -> Maybe (POp,[Asm])
-parsePOp = \case
-  [] -> Nothing
-  asm : asms ->
-    case asm of
-      Opcode op 
-        --push, dup, swap, pop, label
-        | 'p':'u':'s':'h':strn <- op ->
-            let len = read strn
-                (const,asms') = parseImmArgs len asms
-            in Just (PPush len const, asms')
-        | 'd':'u':'p':strn <- op ->
-            let n = read strn
-            in Just (PDup n, asms)
-        | 's':'w':'a':'p':strn <- op ->
-            let n = read strn
-            in Just (PSwap n, asms)
-        | "pop" <- op -> Just (PPop,asms)
-      PlaceLabel lab -> Just (PPlaceLabel lab, asms)
-      _ -> Nothing
+{-
+Bugfix:
+lab1:
+push0
+lab2:
+jumpdest
+
+incorrectly lifted lab2:
+
+lab1:
+lab2:
+push0
+jumpdest
+
+changing its meaning. Fix: remove placelabel from POp, collect labels
+separately, successfully parse only if there is ultimately a POp
+(push,dup,swap,pop). Then parsePOp parses (PlaceLabel _)*(push|dup|swap|pop).
+Note
+lab1:
+push1 1
+lab2:
+push1 2
+
+will still lift lab2, meaning lab2 points to push1 1 rather than 2.
+Since lab2 is not followed by a jumpdest, that can only occur if lab1 falls
+through to lab2, which means that lab2 has no other callers.
+That means lifting it won't break behavior; I still leave it in to make the
+asm more readable and avoid dropping any label.
+Beware: just because an EVMC asm contains a function label does not mean you
+can link the asm into another program and use that function!
+TODO support library compilation mode, marking functions as exported.
+-}
+parsePOp :: [Asm] -> Maybe (POp,Set Label,[Asm])
+parsePOp = go S.empty
+  where go labs = \case
+          [] -> Nothing
+          asm : asms ->
+            case asm of
+              Opcode op 
+              --push, dup, swap, pop, label
+                | 'p':'u':'s':'h':strn <- op ->
+                  let len = read strn
+                      (const,asms') = parseImmArgs len asms
+                  in Just (PPush len const, labs, asms')
+                | 'd':'u':'p':strn <- op ->
+                  let n = read strn
+                  in Just (PDup n, labs, asms)
+                | 's':'w':'a':'p':strn <- op ->
+                  let n = read strn
+                  in Just (PSwap n, labs, asms)
+                | "pop" <- op -> Just (PPop,labs,asms)
+              PlaceLabel lab -> go (S.insert lab labs) asms
+              _ -> Nothing
 --Parses exactly n bytes of PUSH* immediate argument off asm.
 --Accepts Bytes bs where |bs| <= m and UseLabel len lab where len <= m
 --where m is the remaining number of bytes to parse;
